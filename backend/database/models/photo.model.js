0fbee27419717e8273e834ad7bd7afaa907a9aef
aashisrjl <aashis252@gmail.com>
2025-02-08 09:27:52 +0545

    update: env file

--- backend/database/models/photo.model.js
+++ backend/database/models/photo.model.js
@@ -1,31 +1,29 @@
 const mongoose = require("mongoose");
 const validator = require("validator");
-const photoSchema = new mongoose.Schema({
-
-    Phototype:{
-        type:String,
-        enum:['Personal','Shared']
+const photoSchema = new mongoose.Schema(
+  {
+    Phototype: {
+      type: String,
+      enum: ["Personal", "Shared"],
     },
-    Url:{
-        type: string,
-        validate(value){
-            if(!validator.isURL(value)){
-                throw new Error("Invalid Photo Url:",value)
-
-            }
+    Url: {
+      type: string,
+      validate(value) {
+        if (!validator.isURL(value)) {
+          throw new Error("Invalid Photo Url:", value);
         }
+      },
     },
-    caption:{
-        type: String,
+    caption: {
+      type: String,
     },
-    userId:{
-        type: mongoose.Schema.Types.ObjectId,
-        Ref: 'User',
-        required: true
-    }
-
-},
-{timestamps:true});
-  const Photo = mongoose.model("Photo", photoSchema);
-  module.exports = Photo;
-  
+    userId: {
+      type: mongoose.Schema.Types.ObjectId,
+      Ref: "User",
+      required: true,
+    },
+  },
+  { timestamps: true }
+);
+const Photo = mongoose.model("Photo", photoSchema);
+module.exports = Photo;
